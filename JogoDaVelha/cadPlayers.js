5e03113ed4a05f4e76377085fcea06b06a52fac0
Nicolas Amaral <133721626+devNicolasAmaral@users.noreply.github.com>
2023-12-09 10:54:54 -0300

    Add files via upload

---
+++ JogoDaVelha/cadPlayers.js
@@ -0,0 +1,13 @@
+let playerX_Name = document.getElementById("playerX_Name");
+let playerX_Btn = document.getElementById("playerX_Btn");
+let cadPlayerX = document.getElementById("cadPlayerX");
+
+const cadName = () => {
+    playerX_Btn.addEventListener("click", showName);
+}
+
+function showName(){
+
+}
+
+cadName();
